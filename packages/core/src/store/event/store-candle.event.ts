1f9e7a8dc6798502347f466ea4ee5b4ff3e69e8c
Majchrzak <majchrzakmail@gmail.com>
2021-11-24 20:47:13 +0100

    fix build

--- packages/core/src/store/event/store-candle.event.ts
+++ packages/core/src/store/event/store-candle.event.ts
@@ -1,6 +1,6 @@
 import { TradePatchEventHandler } from './store-trade.event';
 import { timestamp } from '../../common/datetime';
-import { InstrumentSelector } from '../../domain';
+import { InstrumentSelector, Orderbook, Trade } from '../../domain';
 import { State } from '../store.state';
 import { StoreEvent } from './store.event';
 import { event } from '../../common/topic';
@@ -25,30 +25,31 @@ export class CandleEvent implements StoreEvent {
 export function CandleEventHandler(event: CandleEvent, state: State) {
   const instrument = state.universe.instrument[event.instrument.toString()];
 
-  return [
-    // patch trade object
-    TradePatchEventHandler(
-      {
-        type: 'trade-patch',
-        instrument: event.instrument,
-        quantity: event.volume,
-        rate: event.close,
-        timestamp: event.timestamp
-      },
-      state
-    ),
-    // patch orderbook by assuming candle close price is mid orderbook price
-    OrderbookPatchEventHandler(
-      {
-        type: 'orderbook-patch',
-        instrument: event.instrument,
-        bestAskQuantity: event.volume * 0.5,
-        bestAskRate: event.close + instrument.quote.tickSize,
-        bestBidQuantity: event.volume * 0.5,
-        bestBidRate: event.close - instrument.quote.tickSize,
-        timestamp: event.timestamp
-      },
-      state
-    )
-  ];
+  // patch trade object
+  const trade: Trade = TradePatchEventHandler(
+    {
+      type: 'trade-patch',
+      instrument: event.instrument,
+      quantity: event.volume,
+      rate: event.close,
+      timestamp: event.timestamp
+    },
+    state
+  );
+
+  // patch orderbook by assuming candle close price is mid orderbook price
+  const orderbook: Orderbook = OrderbookPatchEventHandler(
+    {
+      type: 'orderbook-patch',
+      instrument: event.instrument,
+      bestAskQuantity: event.volume * 0.5,
+      bestAskRate: event.close + instrument.quote.tickSize,
+      bestBidQuantity: event.volume * 0.5,
+      bestBidRate: event.close - instrument.quote.tickSize,
+      timestamp: event.timestamp
+    },
+    state
+  );
+
+  return [trade, orderbook];
 }
