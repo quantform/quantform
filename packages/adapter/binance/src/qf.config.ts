797be1a8dabf7f89f68bb04a4729a026f30724c3
Mateusz <majchrzakmail@gmail.com>
2022-01-22 08:28:33 +0100

    fix build

--- packages/adapter/binance/src/qf.config.ts
+++ packages/adapter/binance/src/qf.config.ts
@@ -1,10 +1,5 @@
 import { instrumentOf, Order, run, task } from '@quantform/core';
-import { tap } from 'rxjs';
 import { BinanceAdapter } from './binance.adapter';
-
-task('listen-to-orders', session => {
-  return session.pending().pipe(tap(it => console.log(it)));
-});
 
 task('order-new', session => {
   return session.open(Order.buyLimit(instrumentOf('binance:mana-usdt'), 20, 2.5));
