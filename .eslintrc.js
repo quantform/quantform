cae0d7bc24a272a74a2cff378a587b90f0dd2439
Mateusz <majchrzakmail@gmail.com>
2022-03-05 20:57:14 +0100

    upgrade eslint rules

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,22 +1,36 @@
 module.exports = {
-  parser: '@typescript-eslint/parser',
-  parserOptions: {
-    project: 'tsconfig.json',
-    sourceType: 'module'
-  },
-  plugins: ['@typescript-eslint/eslint-plugin'],
-  extends: [
-    'plugin:@typescript-eslint/eslint-recommended',
-    'plugin:@typescript-eslint/recommended',
-    'prettier',
-    'prettier/@typescript-eslint'
-  ],
   root: true,
   env: {
     node: true,
     jest: true
   },
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    project: 'tsconfig.json',
+    sourceType: 'module',
+    tsconfigRootDir: __dirname
+  },
+  plugins: ['@typescript-eslint/eslint-plugin', 'simple-import-sort', 'prettier'],
+  extends: [
+    'plugin:@typescript-eslint/eslint-recommended',
+    'plugin:@typescript-eslint/recommended',
+    'prettier'
+  ],
   rules: {
+    'prettier/prettier': 'error',
+    complexity: ['error', { max: 10 }],
+    'accessor-pairs': ['error'],
+    'arrow-body-style': ['error', 'as-needed'],
+    'simple-import-sort/imports': [
+      'error',
+      {
+        groups: [['^\\u0000'], ['^@?\\w'], ['^components(/.*|$)'], ['^\\.']]
+      }
+    ],
+    curly: 'error',
+    'dot-notation': 'error',
+    'capitalized-comments': ['error'],
+    'no-underscore-dangle': 'error',
     '@typescript-eslint/interface-name-prefix': 'off',
     '@typescript-eslint/explicit-function-return-type': 'off',
     '@typescript-eslint/explicit-module-boundary-types': 'off',
