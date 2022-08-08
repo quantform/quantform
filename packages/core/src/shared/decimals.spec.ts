792e6d7581bfd3b4c108459f297cff68f9c73534
Mateusz <majchrzakmail@gmail.com>
2022-08-08 20:43:58 +0200

    fixed decimal tests

--- packages/core/src/shared/decimals.spec.ts
+++ packages/core/src/shared/decimals.spec.ts
@@ -9,20 +9,14 @@ describe('decimals', () => {
     expect(d(2).decimalPlaces()).toEqual(0);
   });
 
-  test('trunc tests', () => {
-    expect(d(0.01).toFloor(2)).toEqual(d(0.01));
-    expect(d(0.011).toFloor(2)).toEqual(d(0.01));
-    expect(d(0.019).toFloor(2)).toEqual(d(0.01));
-    expect(d(0.019).toFloor(0)).toEqual(d(0));
-    expect(d(0.00000058).toFloor(8)).toEqual(d(0.00000058));
-  });
-
   test('floor tests', () => {
     expect(d(0.01).toFloor(2)).toEqual(d(0.01));
     expect(d(0.000000019).toFloor(8)).toEqual(d(0.00000001));
     expect(d(0.011).toFloor(2)).toEqual(d(0.01));
     expect(d(0.019).toFloor(2)).toEqual(d(0.01));
     expect(d(0.019).toFloor(0)).toEqual(d(0));
+    expect(d(0.01).toFloor(2)).toEqual(d(0.01));
+    expect(d(0.00000058).toFloor(8)).toEqual(d(0.00000058));
   });
 
   test('ceil tests', () => {
