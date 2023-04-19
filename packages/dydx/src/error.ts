72a96f232653905bfe706928b7d0bae7d1432a10
Majchrzak <majchrzakmail@gmail.com>
2023-04-19 19:15:14 +0200

    feat: build pipeline for beta

--- packages/dydx/src/error.ts
+++
@@ -1,7 +0,0 @@
-import { InstrumentSelector } from '@quantform/core';
-
-export class InstrumentNotFoundError extends Error {
-  constructor(selector: InstrumentSelector) {
-    super(`Instrument not found: ${selector.id}`);
-  }
-}
