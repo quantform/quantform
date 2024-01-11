b9c9e096c0fffac19b96b96513b8470c8c66c3f0
Majchrzak <majchrzakmail@gmail.com>
2024-01-11 16:34:37 +0100

    feat(core): add `onExit` script handler

--- packages/core/src/cli/internal/script.ts
+++ packages/core/src/cli/internal/script.ts
@@ -1,5 +1,14 @@
 import { join } from 'path';
-import { lastValueFrom } from 'rxjs';
+import {
+  finalize,
+  firstValueFrom,
+  fromEvent,
+  last,
+  merge,
+  of,
+  switchMap,
+  take
+} from 'rxjs';
 
 import { core } from '@lib/core';
 import { Dependency, Module } from '@lib/module';
@@ -12,12 +21,29 @@ export class Script {
     private readonly dependencies: Dependency[]
   ) {}
 
-  async runWhileFinished() {
+  async run() {
     const script = await import(join(buildDirectory(), this.name));
     const module = new Module([...core(), ...script.onInstall(), ...this.dependencies]);
 
     const { act } = await module.awake();
 
-    return await act(() => lastValueFrom(script.onAwake()));
+    return await act(() => {
+      process.stdin.resume();
+
+      return firstValueFrom(
+        merge(
+          script.onAwake().pipe(last()),
+          fromEvent(process, 'exit'),
+          fromEvent(process, 'SIGINT'),
+          fromEvent(process, 'SIGUSR1'),
+          fromEvent(process, 'SIGUSR2'),
+          fromEvent(process, 'uncaughtException')
+        ).pipe(
+          take(1),
+          switchMap(it => script.onExit() ?? of(it)),
+          finalize(() => process.exit(0))
+        )
+      );
+    });
   }
 }
