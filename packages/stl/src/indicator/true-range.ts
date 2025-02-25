71968ff38b58aea0994a0373187a425f3deeb4c4
Majchrzak <majchrzakmail@gmail.com>
2025-02-25 20:30:35 +0100

    fix(pipeline): fix build

--- packages/stl/src/indicator/true-range.ts
+++ packages/stl/src/indicator/true-range.ts
@@ -4,22 +4,11 @@ import { decimal, Ohlc } from '@quantform/core';
 
 export function trueRange<T>(fn: (it: T) => Ohlc) {
   return function (source: Observable<T>): Observable<[T, decimal]> {
-    let previous: Ohlc;
-
     return source.pipe(
       map(it => {
         const current = fn(it);
 
-        const value =
-          previous == null
-            ? current.high.minus(current.low)
-            : decimal.max(
-                decimal.max(
-                  current.high.minus(current.low),
-                  decimal.abs(current.high.minus(previous.close))
-                ),
-                decimal.abs(current.low.minus(previous.close))
-              );
+        const value = current.high.minus(current.low);
 
         const tuple: [T, decimal] = [it, value];
 
