aa30f16bc0e834fba3eb7acf2a19c84481f29e0d
Majchrzak <majchrzakmail@gmail.com>
2025-11-30 15:42:11 +0100

    feat: change telegram deps

--- packages/telegram/src/use-bot.ts
+++ packages/telegram/src/use-bot.ts
@@ -1,9 +1,11 @@
+import { Bot } from 'grammy';
+
 import { useMemo } from '@quantform/core';
-import TelegramBot = require('node-telegram-bot-api');
+
 import { useOptions } from './use-options';
 
 export function useBot() {
-  const { token, polling } = useOptions();
+  const { token } = useOptions();
 
-  return useMemo(() => new TelegramBot(token, { polling }), ['telegram', 'telegram-bot']);
+  return useMemo(() => new Bot(token), ['telegram', 'telegram-bot']);
 }
